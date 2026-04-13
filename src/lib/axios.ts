352fafb2ac25d18c35efc859a67f86ac516f03c8
Mamun-Hossain-Dev <mh4559641@gmail.com>
2026-04-13 13:52:22 +0600

    fix the axios thing

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const apiBaseUrl = (process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000")
+    .replace(/\/+$/, "")
+    .replace(/\/api\/v1$/, "");
+
 export const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+    baseURL: apiBaseUrl,
     headers: {
         "Content-Type": "application/json",
     },
